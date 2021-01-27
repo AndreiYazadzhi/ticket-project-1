9f865fbe612b6fa81cb7474bf8fe5a6be8efd07c
AndreiYazadzhi <belator251000@gmail.com>
2021-01-27 12:31:57 +0200

    rest

--- src/main/java/com/hibernate/dao/impl/MovieDaoImpl.java
+++ src/main/java/com/hibernate/dao/impl/MovieDaoImpl.java
@@ -18,7 +18,9 @@ public class MovieDaoImpl implements MovieDao {
         Session session = null;
         try {
             session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
             session.save(movie);
+            transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
