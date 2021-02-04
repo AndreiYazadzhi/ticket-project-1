b1d3019f48dcf7dba3723f988274814164cf2277
AndreiYazadzhi <belator251000@gmail.com>
2021-02-04 19:29:29 +0200

    Minor fix

--- src/main/java/com/hibernate/service/impl/OrderServiceImpl.java
+++ src/main/java/com/hibernate/service/impl/OrderServiceImpl.java
@@ -1,15 +1,14 @@
 package com.hibernate.service.impl;
 
 import com.hibernate.dao.OrderDao;
-import com.hibernate.dao.ShoppingCartDao;
 import com.hibernate.lib.Inject;
 import com.hibernate.lib.Service;
 import com.hibernate.model.Order;
 import com.hibernate.model.ShoppingCart;
 import com.hibernate.model.User;
 import com.hibernate.service.OrderService;
+import com.hibernate.service.ShoppingCartService;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,7 +16,7 @@ public class OrderServiceImpl implements OrderService {
     @Inject
     private OrderDao orderDao;
     @Inject
-    private ShoppingCartDao shoppingCartDao;
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public Order completeOrder(ShoppingCart shoppingCart) {
@@ -26,8 +25,7 @@ public class OrderServiceImpl implements OrderService {
         order.setTickets(shoppingCart.getTickets());
         order.setUser(shoppingCart.getUser());
         orderDao.add(order);
-        shoppingCart.setTickets(new ArrayList<>());
-        shoppingCartDao.update(shoppingCart);
+        shoppingCartService.clear(shoppingCart);
         return order;
     }
 
