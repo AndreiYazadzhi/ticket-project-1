aac5e64f42008bde00cb0576bc61a74e156a4203
AndreiYazadzhi <belator251000@gmail.com>
2021-02-04 14:38:57 +0200

    Relations updates

--- src/main/java/com/hibernate/model/Order.java
+++ src/main/java/com/hibernate/model/Order.java
@@ -2,13 +2,7 @@ package com.hibernate.model;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "orders")
@@ -16,10 +10,12 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @OneToMany
+    @OneToMany(fetch = FetchType.LAZY)
     private List<Ticket> tickets;
+    @Column(name = "order_date")
     private LocalDateTime orderDate;
     @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
     public Long getId() {
