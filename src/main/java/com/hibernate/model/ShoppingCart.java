856ad0c6cfc9470304095aaac6cfc029d31b759d
AndreiYazadzhi <belator251000@gmail.com>
2021-02-04 10:45:02 +0200

    Travis

--- src/main/java/com/hibernate/model/ShoppingCart.java
+++ src/main/java/com/hibernate/model/ShoppingCart.java
@@ -2,8 +2,6 @@ package com.hibernate.model;
 
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
