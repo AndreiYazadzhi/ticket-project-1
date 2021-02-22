5a12e36a462164946d8b92bc300c2e59ab05110b
AndreiYazadzhi <belator251000@gmail.com>
2021-02-22 15:03:33 +0200

    Fixed mistakes

--- src/main/java/com/hibernate/dao/impl/RoleDaoImpl.java
+++ src/main/java/com/hibernate/dao/impl/RoleDaoImpl.java
@@ -8,7 +8,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class RoleDaoImpl implements RoleDao {
     private final SessionFactory sessionFactory;
 
